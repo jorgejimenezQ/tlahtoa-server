bee50e547a65bc3fa82e7251decbf935f9bae730
jorgejimenezQ <jorgejimenez@mail.weber.edu>
2023-03-27 19:13:57 -0600

    Add boilerplate code for the chat server

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+import express from 'express'
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+const httpServer = createServer(app)
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+    },
+})
+
+io.on('connection', (socket) => {
+    console.log('a user connected')
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
+    })
+})
+
+httpServer.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})
